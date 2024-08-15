3a96279e4c9ac2e8534edf63e0c67238e65fb3bf
Robert Sturla <robertsturla@outlook.com>
2024-08-15 19:44:43 +0100

    feat: regenerate initramfs

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,3 +4,9 @@ set -euox pipefail
 
 # Cleanup DNF
 dnf clean all
+
+# Generate initramfs
+mkdir -p /var/tmp
+KERNEL_SUFFIX=""
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+dracut --kver "$QUALIFIED_KERNEL" --reproducible -vf "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
