dfe11dc1116b638ff50517a0cd2d0552b52e3967
Robert Sturla <robertsturla@outlook.com>
2024-08-18 09:11:03 +0100

    fix: move excludes to gnome install

--- scripts/_silverblue/001-desktop-environment.sh
+++ scripts/_silverblue/001-desktop-environment.sh
@@ -3,5 +3,44 @@
 set -euox pipefail
 
 # Install the desktop environment
-dnf install -y @gnome-desktop
+excludes=(
+
+    "baobab"
+    "cheese"
+    "evince"
+    "evince-djvu"
+    "evince-nautilus"
+    "file-roller"
+    "file-roller-nautilus"
+    "gnome-boxes"
+    "gnome-calculator"
+    "gnome-calendar"
+    "gnome-characters"
+    "gnome-clocks"
+    "gnome-connections"
+    "gnome-contacts"
+    "gnome-documents"
+    "gnome-font-viewer"
+    "gnome-logs"
+    "gnome-maps"
+    "gnome-photos"
+    "gnome-screenshot"
+    "gnome-text-editor"
+    "gnome-weather"
+    "jwhois"
+    "loupe"
+    "rdist"
+    "sane-backends-drivers-scanners"
+    "simple-scan"
+    "snapshot"
+    "sushi"
+    "symlinks"
+    "tcpdump"
+    "telnet"
+    "totem"
+    "totem-nautilus"
+    "traceroute"
+)
+
+dnf install -y @gnome-desktop --exclude "${excludes[*]}"
 systemctl enable gdm.service
